0e7ecc657db6b1efe34204446178b9ce345d9f66
valentinhippolyte <74503837+valentinhippolyte@users.noreply.github.com>
2025-04-04 14:21:53 +0200

    update hero section: photo + begining text

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col sm:flex-row items-center justify-evenly mt-10">
+    <div>
+      <img
+        src="../assets/photo.jpg"
+        class="w-48 h-48 sm:w-72 sm:h-72 rounded-full"
+      />
+    </div>
+
+    <div class="text-white text-center sm:text-left">
+      <p class="text-2xl sm:text-3xl">Hi there!</p>
+      <p class="text-4xl sm:text-5xl">
+        I'm
+        <span class="font-bold text-app-green">
+          Valentin
+          <span class="hidden lg:inline-block">HIPPOLYTE</span>
+        </span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
--- src/screens/HomeScreen.vue
+++ src/screens/HomeScreen.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="bg-gray-900">
     <Nav />
+    <HeroSection />
   </div>
 </template>
 
 <script setup>
+import HeroSection from "../components/HeroSection.vue";
 import Nav from "../components/Nav.vue";
 </script>
