683c01c99b03b14dbdbbe4ad5fe50121edf03f41
valentinhippolyte <74503837+valentinhippolyte@users.noreply.github.com>
2025-04-08 15:55:42 +0200

    social links added

--- src/screens/ContactScreen.vue
+++ src/screens/ContactScreen.vue
@@ -6,7 +6,7 @@
     />
 
     <div
-      class="flex flex-col gap-16 lg:flex-row lg:justify-around pt-8 sm:pt-16 md:pt-26 lg:pt-40"
+      class="flex flex-col gap-16 lg:flex-row lg:justify-around pt-16 md:pt-26 lg:pt-40"
     >
       <div
         class="flex flex-col sm:flex-row justify-around items-center gap-10 w-full"
@@ -136,50 +136,57 @@
           </h4>
           <div class="flex gap-5">
             <!-- Icon -->
-            <div
-              class="size-12 rounded-full overflow-hidden flex items-center bg-white p-2"
+            <a
+              href="https://www.linkedin.com/in/valentin-hippolyte/"
+              target="_blank"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="90"
-                height="90"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="#117f7e"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-linkedin-icon lucide-linkedin"
+              <div
+                class="size-12 rounded-full overflow-hidden flex items-center bg-white p-2 transition-transform duration-300 ease-in-out hover:scale-105 hover:-translate-y-1 hover:cursor-pointer"
               >
-                <path
-                  d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
-                />
-                <rect width="4" height="12" x="2" y="9" />
-                <circle cx="4" cy="4" r="2" />
-              </svg>
-            </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="90"
+                  height="90"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#117f7e"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-linkedin-icon lucide-linkedin"
+                >
+                  <path
+                    d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+                  />
+                  <rect width="4" height="12" x="2" y="9" />
+                  <circle cx="4" cy="4" r="2" />
+                </svg>
+              </div>
+            </a>
             <!-- Icon -->
-            <div
-              class="size-12 rounded-full overflow-hidden flex items-center bg-white p-2"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="90"
-                height="90"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="#117f7e"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-github-icon lucide-github"
+            <a href="https://github.com/valentinhippolyte" target="_blank">
+              <div
+                class="size-12 rounded-full overflow-hidden flex items-center bg-white p-2 transition-transform duration-300 ease-in-out hover:scale-105 hover:-translate-y-1 hover:cursor-pointer"
               >
-                <path
-                  d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
-                />
-                <path d="M9 18c-4.51 2-5-2-7-2" />
-              </svg>
-            </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="90"
+                  height="90"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#117f7e"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-github-icon lucide-github"
+                >
+                  <path
+                    d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
+                  />
+                  <path d="M9 18c-4.51 2-5-2-7-2" />
+                </svg>
+              </div>
+            </a>
           </div>
         </div>
       </div>
