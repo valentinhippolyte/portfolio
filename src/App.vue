a3338b09d4bd62cfe85c6a2f6e5b6a1bd5121123
valentinhippolyte <74503837+valentinhippolyte@users.noreply.github.com>
2025-04-08 15:48:42 +0200

    contact responsive done

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <div class="scroll-smooth">
     <HomeScreen />
     <AboutMeScreen />
+    <ContactScreen />
   </div>
 </template>
 
 <script setup>
 import AboutMeScreen from "./screens/AboutMeScreen.vue";
+import ContactScreen from "./screens/ContactScreen.vue";
 import HomeScreen from "./screens/HomeScreen.vue";
 </script>
