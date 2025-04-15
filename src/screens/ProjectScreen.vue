8eac5212639b73414d85716afd9a1eb1470ac139
valentinhippolyte <74503837+valentinhippolyte@users.noreply.github.com>
2025-04-15 10:15:19 +0200

    fix some style in project screen

--- src/screens/ProjectScreen.vue
+++ src/screens/ProjectScreen.vue
@@ -1,150 +1,197 @@
 <template>
   <section class="bg-app-second-bg px-6">
-    <Title
-      :firstTitle="currentProject?.firstTitle"
-      :secondTitle="currentProject?.secondTitle"
-    />
-
-    <div
-      class="flex flex-col lg:flex-row items-center justify-between gap-10 mt-10 max-w-6xl mx-auto"
-    >
-      <!-- Image -->
-      <div class="w-full md:w-1/2 flex justify-center">
-        <img
-          :src="currentProject?.image"
-          class="rounded-xl object-cover shadow-lg max-w-full h-auto sm:h-72 md:h-80 lg:h-96"
-          alt="Project Image"
-        />
+    <div class="max-w-6xl mx-auto px-6">
+      <div class="w-full md:w-2/3 px-2 pt-8 sm:pt-16 md:pt-26 lg:pt-40">
+        <h2 class="text-4xl font-bold text-app-orange">
+          {{ currentProject?.firstTitle }}
+          <span class="text-white">{{ currentProject?.secondTitle }}</span>
+        </h2>
+        <div class="bg-app-green h-1.5 w-13 mt-1"></div>
       </div>
 
-      <!-- Texte -->
-      <div class="w-full md:w-1/2 text-white space-y-4 text-justify">
-        <p class="text-base md:text-lg leading-relaxed">
-          {{ currentProject?.description }}
-        </p>
-
-        <div class="flex items-center gap-4 flex-wrap">
-          <Skill
-            v-for="skill in currentProject?.stacks"
-            :src="skill.image"
-            :skillName="skill.skillName"
+      <div
+        class="flex flex-col lg:flex-row items-center justify-between gap-10 mt-10 max-w-6xl mx-auto"
+      >
+        <!-- Image -->
+        <div class="w-full md:w-2/3 flex justify-center">
+          <img
+            :src="currentProject?.image"
+            class="rounded-xl object-cover shadow-lg max-w-full h-auto sm:h-72 md:h-80 lg:h-96"
+            alt="Project Image"
           />
         </div>
 
-        <div class="flex gap-5">
-          <a :href="currentProject?.github" target="_blank">
-            <button
-              type="button"
-              class="text-white bg-app-green hover:bg-app-green-600 hover:scale-105 transition-all duration-300 ease-in-out hover:cursor-pointer rounded-2xl text-sm px-3 py-2 text-center inline-flex items-center"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-github-icon lucide-github"
+        <!-- Texte -->
+        <div class="w-full md:w-2/3 text-white space-y-4 text-justify">
+          <p class="text-base md:text-lg leading-relaxed">
+            {{ currentProject?.description }}
+          </p>
+
+          <div class="flex items-center gap-4 flex-wrap">
+            <Skill
+              v-for="skill in currentProject?.stacks"
+              :src="skill.image"
+              :skillName="skill.skillName"
+            />
+          </div>
+
+          <div class="flex gap-5">
+            <a :href="currentProject?.github" target="_blank">
+              <button
+                type="button"
+                class="text-white bg-app-green hover:bg-app-green-600 hover:scale-105 transition-all duration-300 ease-in-out hover:cursor-pointer rounded-2xl text-sm px-3 py-2 text-center inline-flex items-center"
               >
-                <path
-                  d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
-                />
-                <path d="M9 18c-4.51 2-5-2-7-2" />
-              </svg>
-              &nbsp; github
-            </button>
-          </a>
-          <a
-            v-if="currentProject?.site"
-            :href="currentProject?.github"
-            target="_blank"
-          >
-            <button
-              type="button"
-              class="text-white bg-app-green hover:bg-app-green-600 hover:scale-105 transition-all duration-300 ease-in-out hover:cursor-pointer rounded-2xl text-sm px-3 py-2 text-center inline-flex items-center"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-github-icon lucide-github"
+                >
+                  <path
+                    d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
+                  />
+                  <path d="M9 18c-4.51 2-5-2-7-2" />
+                </svg>
+                &nbsp; github
+              </button>
+            </a>
+            <a
+              v-if="currentProject?.site"
+              :href="currentProject?.github"
+              target="_blank"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="lucide lucide-globe-icon lucide-globe"
+              <button
+                type="button"
+                class="text-white bg-app-green hover:bg-app-green-600 hover:scale-105 transition-all duration-300 ease-in-out hover:cursor-pointer rounded-2xl text-sm px-3 py-2 text-center inline-flex items-center"
               >
-                <circle cx="12" cy="12" r="10" />
-                <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20" />
-                <path d="M2 12h20" />
-              </svg>
-              &nbsp; site
-            </button>
-          </a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-globe-icon lucide-globe"
+                >
+                  <circle cx="12" cy="12" r="10" />
+                  <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20" />
+                  <path d="M2 12h20" />
+                </svg>
+                &nbsp; site
+              </button>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
 
-    <!-- Features & Challenges -->
-    <div
-      class="flex flex-col lg:flex-row items-center justify-between gap-10 mt-10 max-w-6xl mx-auto"
-    >
-      <!-- Features -->
-      <div class="w-full md:w-1/2 lg:w-1/2 text-white">
-        <h3 class="text-2xl font-bold pb-3">Main Features</h3>
-        <ul
-          class="list-disc list-inside marker:text-app-green space-y-2 text-base sm:text-xl"
-        >
-          <li v-for="feature in currentProject?.features" :key="feature">
-            {{ feature }}
-          </li>
-        </ul>
-      </div>
-
-      <!-- Challenges -->
+      <!-- Features & Challenges -->
       <div
-        class="w-full md:w-1/2 lg:w-1/2 text-white self-center lg:self-start"
+        class="flex flex-col lg:flex-row items-center justify-between gap-10 mt-10 max-w-6xl mx-auto"
       >
-        <h3 class="text-2xl font-bold pb-3">Défis techniques</h3>
-        <p class="text-base sm:text-lg leading-relaxed">
-          {{ currentProject?.challenges }}
-        </p>
+        <!-- Features -->
+        <div class="w-full md:w-2/3 lg:w-1/2 text-white">
+          <h3 class="text-2xl font-bold pb-3">Main Features</h3>
+          <ul
+            class="list-disc list-outside pl-4 marker:text-app-green space-y-2 text-base sm:text-xl"
+          >
+            <li v-for="feature in currentProject?.features" :key="feature">
+              {{ feature }}
+            </li>
+          </ul>
+        </div>
+
+        <!-- Challenges -->
+        <div
+          class="w-full md:w-2/3 lg:w-1/2 text-white self-center lg:self-start"
+        >
+          <h3 class="text-2xl font-bold pb-3">Défis techniques</h3>
+          <p class="text-base sm:text-lg leading-relaxed">
+            {{ currentProject?.challenges }}
+          </p>
+        </div>
       </div>
-    </div>
 
-    <!-- Navigation entre projets -->
-    <div
-      class="mt-16 flex justify-between items-center max-w-6xl mx-auto text-white"
-    >
-      <router-link
-        v-if="prevProject"
-        :to="`/projects/${prevProject.id}`"
-        class="hover:underline text-white"
+      <!-- Navigation entre projets + retour -->
+      <div
+        class="mt-16 flex justify-between items-center max-w-6xl mx-auto text-white relative"
       >
-        ← {{ prevProject.firstTitle }} {{ prevProject.secondTitle || "" }}
-      </router-link>
+        <!-- Colonne gauche -->
+        <div class="flex-1">
+          <router-link
+            v-if="prevProject"
+            :to="`/projects/${prevProject.id}`"
+            class="hover:underline text-white"
+          >
+            <div class="flex gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#ff8d20"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-circle-arrow-left-icon lucide-circle-arrow-left"
+              >
+                <circle cx="12" cy="12" r="10" />
+                <path d="M16 12H8" />
+                <path d="m12 8-4 4 4 4" />
+              </svg>
+              {{ "previous" || "" }}
+            </div>
+          </router-link>
+        </div>
 
-      <router-link
-        v-if="nextProject"
-        :to="`/projects/${nextProject.id}`"
-        class="hover:underline text-white ml-auto"
-      >
-        {{ nextProject.firstTitle }} {{ nextProject.secondTitle || "" }} →
-      </router-link>
-    </div>
+        <!-- Colonne centre -->
+        <div class="flex-none">
+          <router-link
+            to="/"
+            class="inline-block bg-app-green text-white px-5 py-2 rounded-2xl hover:bg-app-green-600 transition-all"
+          >
+            profil
+          </router-link>
+        </div>
 
-    <!-- Retour à l'accueil -->
-    <div class="mt-6 text-center">
-      <router-link
-        to="/"
-        class="inline-block bg-app-green text-white px-5 py-2 rounded-2xl hover:bg-app-green-600 transition-all"
-      >
-        Retour au profil
-      </router-link>
+        <!-- Colonne droite -->
+        <div class="flex-1 text-right">
+          <router-link
+            v-if="nextProject"
+            :to="`/projects/${nextProject.id}`"
+            class="hover:underline text-white"
+          >
+            <div class="flex justify-end gap-1">
+              {{ "next" || "" }}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#ff8d20"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-circle-arrow-left-icon lucide-circle-arrow-left rotate-180"
+              >
+                <circle cx="12" cy="12" r="10" />
+                <path d="M16 12H8" />
+                <path d="m12 8-4 4 4 4" />
+              </svg>
+            </div>
+          </router-link>
+        </div>
+      </div>
     </div>
   </section>
 
@@ -157,7 +204,6 @@ import { useI18n } from "vue-i18n";
 import { useRoute } from "vue-router";
 import Rights from "../components/Rights.vue";
 import Skill from "../components/Skill.vue";
-import Title from "../components/title.vue";
 
 const { t } = useI18n();
 
